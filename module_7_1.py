c0965fbf727db73e331109908b33b8d60083d1c3
Игорь Рябухин <igorr1970@mail.ru>
2024-11-06 21:36:06 +0500

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -5,36 +5,45 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
+        return f"{self.name}, {self.weight}, {self.category}"
 
 class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
 
     def get_products(self):
-        try:
-            with open(self.__file_name, 'r') as file:
-                return file.read().strip()
-        except FileNotFoundError:
-            return ''
+        products = ''
+        file = open(self.__file_name, 'r')
+        products = file.read()
+        file.close()
+        return products
 
     def add(self, *products):
-        existing_products = self.get_products().splitlines()
-        existing_names = {product.split(', ')[0] for product in existing_products}
+        existing_products = self.get_products().split('\n')
+        existing_names = []
 
+        for product in existing_products:
+            if product:
+                product_info = product.split(', ')
+                product_name = product_info[0]
+                existing_names.append(product_name)
+
+        file = open(self.__file_name, 'a')
         for product in products:
-            if product.name in existing_names:
-                print(f'Продукт {product} уже есть в магазине')
+            if product.name not in existing_names:
+                file.write(str(product) + '\n')
+                existing_names.append(product.name)
             else:
-                with open(self.__file_name, 'a') as file:
-                    file.write(str(product) + '\n')
+                print(f"Продукт {product.name} уже есть в магазине")
+        file.close()
 
+# Пример работы программы:
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)
+print(p2) # __str__
 
 s1.add(p1, p2, p3)
 
