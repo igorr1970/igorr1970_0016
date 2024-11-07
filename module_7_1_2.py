e0ad16d54861573f1ff271d6ee94f3dccec8b984
Игорь Рябухин <igorr1970@mail.ru>
2024-11-07 21:56:35 +0500

    Update module_7_1_2.py

--- module_7_1_2.py
+++ module_7_1_2.py
@@ -30,13 +30,12 @@ class Shop:
                     print(f"Продукт '{product}' уже есть в магазине")
 
 
-# Пример работы программы:
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)  # выводим с помощью __str__
+print(p2)
 
 s1.add(p1, p2, p3)
 
